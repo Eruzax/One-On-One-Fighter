3443d933fa23213725c6505b616f9a8b3e9de171
Kaden <coolbean1123@yahoo.com>
2022-12-06 23:37:02 -0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function initialize()
 {
-    atk = document.getElementById(attack);
-    def = document.getElementsById(defend);
-    fm = document.getElementById(finishingMove);
+    playerHealth = documnet.getElementById("playerhealth");
+    enemyHealth = document.getElementById("enemyhealth");
+    pHealth = 100;
+    eHealth = 100;
+
+    display();
+}
+
+function turn()
+{
+    if (ehealth !=0 && pHealth != 0)
+    {
+        
+    }
+    else
+    {
+        if (pHealth <= 0)
+        {
+            console.log("Enemy Wins")
+        }
+        else if (eHealth <= 0)
+        {
+            console.log("Player Wins")
+        }
+    }
+}
+
+function attack()
+{
+
+}
+
+function display()
+{
+    playerHealth.innerHTML = pHealth;
+    enemyHealth.innerHTML = eHealth;
 }
