671465225da57b23f5d3102f45d9367bff2dd5c4
Kaden <coolbean1123@yahoo.com>
2022-12-10 16:25:26 -0500

    update

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function initialize()
     enemySpeed = document.getElementById("enemyspeed");
     enemyFatigue = document.getElementById("enemyfatigue");
 
-    player = {strength: 6, cunning: 6, speed: 6, fatigue: 30};
-    enemy = {strength: 6, cunning: 6, speed: 6, fatigue: 30};
+    player = {strength: 6, cunning: 6, speed: 6, fatigue: 30, maxFatigue: 30, isAttack: false, isDefending: false, isFinishingMove: false};
+    enemy = {strength: 6, cunning: 6, speed: 6, fatigue: 30, maxFatigue: 30, isAttack: false, isDefending: false, isFinishingMove: false};
 
     playerLog = document.getElementById("playerlog");
     enemyLog = document.getElementById("enemylog");
@@ -22,109 +22,241 @@ function initialize()
     display();
 }
 
-// function player(strength, cunning, speed, fatigue)
-// {
-//     this.strength = 6;
-//     this.cunning = 6;
-//     this.speed = 6;
-//     this.fatigue = 30;
-// }
-
 function play()
 {
-    //modify player stats
-    playerStats = [player.strength, player.cunning, player.speed, player.fatigue]
+    //player stat increase
+    playerStats = [1, 2, 3, 4]
     for (i = 0; i < 2; i++)
     {
-        index = Math.floor(Math.random() * (playerStats.length))
-        if (playerStats[index] == player.fatigue)
+        let index = Math.floor(Math.random() * (playerStats.length))
+        if (playerStats[index] === 1)
         {
-            modifier(player.fatigue, Math.floor(Math.random() * 7));
+            player.strength += Math.floor(Math.random() * 2);
+            console.log("player strength inc: "+ player.strength);
             playerStats.splice(index, 1);
         }
-        else
+        else if (playerStats[index] === 2)
         {
-            modifier(playerStats[index], Math.floor(Math.random() * 2));
+            player.cunning += Math.floor(Math.random() * 2);
+            console.log("player cunning inc: "+ player.cunning);
+            playerStats.splice(index, 1);
+        }
+        else if (playerStats[index] === 3)
+        {
+            player.speed += Math.floor(Math.random() * 2);
+            console.log("player speed inc: "+ player.speed);
+            playerStats.splice(index, 1);
+        }
+        else if (playerStats[index] === 4)
+        {
+            player.fatigue += Math.floor(Math.random() * 7);
+            console.log("player fatigue inc: "+ player.fatigue)
             playerStats.splice(index, 1);
         }
     }
 
-    if (playerStats[0] === player.fatigue)
+    // player stat decrease
+    if (playerStats[0] === 1)
     {
-        modifier(player.fatigue, Math.floor(Math.random() * 7) * -1);
+        player.strength += (Math.floor(Math.random() * 2) * -1);
+        console.log("player strength dec: "+ player.strength);
     }
-    else
+    else if (playerStats[0] === 2)
     {
-        modifier(playerStats[0], Math.floor(Math.random() * 2) * -1);
+        player.cunning += (Math.floor(Math.random() * 2) * -1);
+        console.log("player cunning dec: "+ player.cunning);
+    }
+    else if (playerStats[0] === 3)
+    {
+        player.speed += (Math.floor(Math.random() * 2) * -1);
+        console.log("player speed dec: "+ player.speed);
+    }
+    else if (playerStats[0] === 4)
+    {
+        player.fatigue += (Math.floor(Math.random() * 7) * -1);
+        console.log("player fatigue dec "+ player.fatigue)
     }
 
-    if (playerStats[1] == player.fatigue)
+    if (playerStats[1] === 1)
     {
-        modifier(player.fatigue, Math.floor(Math.random() * 7) * -1);
+        player.strength += (Math.floor(Math.random() * 2) * -1);
+        console.log("player strength dec: "+ player.strength);
     }
-    else
+    else if (playerStats[1] === 2)
     {
-        modifier(playerStats[1], Math.floor(Math.random() * 2) * -1);
+        player.cunning += (Math.floor(Math.random() * 2) * -1);
+        console.log("player cunning dec: "+ player.cunning);
+    }
+    else if (playerStats[1] === 3)
+    {
+        player.speed += (Math.floor(Math.random() * 2) * -1);
+        console.log("player speed dec: "+ player.speed);
+    }
+    else if (playerStats[1] === 4)
+    {
+        player.fatigue += (Math.floor(Math.random() * 7) * -1);
+        console.log("player fatigue dec "+ player.fatigue)
     }
 
-    //modify enemy stats
-    enemyStats = [enemy.strength, enemy.cunning, enemy.speed, enemy.fatigue]
+    // enemy stat increase
+    enemyStats = [1, 2, 3, 4]
     for (i = 0; i < 2; i++)
     {
-        index = Math.floor(Math.random() * (enemyStats.length))
-        console.log(index);
-        console.log(enemyStats[index]);
-        if (enemyStats[index] == enemy.fatigue)
+        let index = Math.floor(Math.random() * (enemyStats.length))
+        if (enemyStats[index] === 1)
         {
-            modifier(enemy.fatigue, Math.floor(Math.random() * 7));
+            enemy.strength += Math.floor(Math.random() * 2);
+            console.log("enemy strength inc: "+ enemy.strength);
             enemyStats.splice(index, 1);
         }
-        else
+        else if (enemyStats[index] === 2)
         {
-            modifier(enemyStats[index], Math.floor(Math.random() * 2));
+            enemy.cunning += Math.floor(Math.random() * 2);
+            console.log("enemy cunning inc: "+ enemy.cunning);
+            enemyStats.splice(index, 1);
+        }
+        else if (enemyStats[index] === 3)
+        {
+            enemy.speed += Math.floor(Math.random() * 2);
+            console.log("enemy speed inc: "+ enemy.speed);
+            enemyStats.splice(index, 1);
+        }
+        else if (enemyStats[index] === 4)
+        {
+            enemy.fatigue += Math.floor(Math.random() * 7);
+            console.log("enemenemy fatigue inc: "+ enemy.fatigue)
             enemyStats.splice(index, 1);
         }
     }
 
-    if (enemyStats[0] == enemy.fatigue)
+    // enemy stat decreases
+    if (enemyStats[0] === 1)
     {
-        modifier(enemy.fatigue, Math.floor(Math.random() * 7) * -1);
+        enemy.strength += (Math.floor(Math.random() * 2) * -1);
+        console.log("enemy strength dec: "+ enemy.strength);
     }
-    else
+    else if (enemyStats[0] === 2)
     {
-        modifier(enemyStats[0], Math.floor(Math.random() * 2) * -1);
+        enemy.cunning += (Math.floor(Math.random() * 2) * -1);
+        console.log("enemy cunning dec: "+ enemy.cunning);
+    }
+    else if (enemyStats[0] === 3)
+    {
+        enemy.speed += (Math.floor(Math.random() * 2) * -1);
+        console.log("enemy speed dec: "+ enemy.speed);
+    }
+    else if (enemyStats[0] === 4)
+    {
+        enemy.fatigue += (Math.floor(Math.random() * 7) * -1);
+        console.log("enemy fatigue dec "+ enemy.fatigue)
     }
 
-    if (enemyStats[1] == enemy.fatigue)
+    if (enemyStats[1] === 1)
     {
-        modifier(enemy.fatigue, Math.floor(Math.random() * 7) * -1);
+        enemy.strength += (Math.floor(Math.random() * 2) * -1);
+        console.log("enemy strength dec: "+ enemy.strength);
     }
-    else
+    else if (enemyStats[1] === 2)
     {
-        modifier(enemyStats[1], Math.floor(Math.random() * 2) * -1);
+        enemy.cunning += (Math.floor(Math.random() * 2) * -1);
+        console.log("enemy cunning dec: "+ enemy.cunning);
     }
+    else if (enemyStats[1] === 3)
+    {
+        enemy.speed += (Math.floor(Math.random() * 2) * -1);
+        console.log("enemy speed dec: "+ enemy.speed);
+    }
+    else if (enemyStats[1] === 4)
+    {
+        enemy.fatigue += (Math.floor(Math.random() * 7) * -1);
+        console.log("enemy fatigue dec "+ enemy.fatigue)
+    }
+
+    player.maxFatigue = player.fatigue;
+    enemy.maxFatigue = enemy.fatigue;
 
     display();
 }
 
-function attack(p, e)
+function turn(p, e)
 {
-    atk = (p.strength);
+    //Attack
+    if (p.isAttack == true)
+    {
+        atk = (p.strength + p.speed + p.cunning / ((Math.floor(Math.random() * 3)) + 1 ));
+        if (e.isDefending == true)
+        {
+         def = e.speed + e.cunning;
+        }
+        else
+        {
+         def = e.speed + ((Math.floor(Math.random() * 6)) + 1 )
+        }
+     
+        if (atk > def)
+        {
+         e.fatigue -= (atk - def)
+        }
+        else
+        {
+            if (e.fatigue < e.maxFatigue)
+            {
+                restore = (Math.floor(Math.random() * 6)) + 1;
+                if ((e.fatigue + restore) > e.maxFatigue)
+                {
+                    e.fatigue = e.maxFatigue;
+                }
+                else
+                {
+                    e.fatigue += restore;
+                }
+            }
+        }
+    }
+
+    //Defending
+    if (p.isDefending == true && e.isDefending == true)
+    {
+        pRestore = (Math.floor(Math.random() * 6)) + 1;
+        if ((p.fatigue + pRestore) > p.maxFatigue)
+        {
+            p.fatigue = p.maxFatigue;
+        }
+        else
+        {
+            p.fatigue += restore;
+        }
+
+        eRestore = (Math.floor(Math.random() * 6)) + 1;
+        if ((e.fatigue + eRestore) > e.maxFatigue)
+        {
+            e.fatigue = e.maxFatigue;
+        }
+        else
+        {
+            e.fatigue += eRestore;
+        }
+   }
+
+   //Finishing Move
+
+
+   display();
+}
+
+function attack(p)
+{
+    p.isAttack = true;
 }
 
 function defend(p)
 {
-
+    p.isDefending = true;
 }
 
-function finishingMove()
+function finishingMove(p)
 {
-
-}
-
-function modifier(s, mod)
-{
-    s += mod;
+    p.finishingMove = true;
 }
 
 function display()
